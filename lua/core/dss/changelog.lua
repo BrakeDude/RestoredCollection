8ea50bb4af01cd0ec8a7d3cbdfbcee471ac99587
BrakeDude <lockbrake@gmail.com>
2026-05-26 15:21:11 +0300

    Version 1.31.2

--- lua/core/dss/changelog.lua
+++ lua/core/dss/changelog.lua
@@ -1,5 +1,12 @@
 local modname = "Restored Collection"
 
+DeadSeaScrollsMenu.AddChangelog(modname, "v1.31.2",[[{{FSIZE2}}fixes
+- fixed illusions getting more than
+one hp
+- fixed illusions collision with bed
+]],
+{"restored", "collection", "", "v1.31.2", "update"}, false, false)
+
 DeadSeaScrollsMenu.AddChangelog(modname, "v1.31.1",[[{{FSIZE2}}fixes
 - fixed eid errors on launch
 ]],
